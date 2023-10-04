93bf0cf078c5a44762d92ad0a076cc0f05067ec5
Tushar Patle <tusharpatle0097@gmail.com>
2023-10-04 23:14:54 +0530

    work in progress

---
+++ src/Routing/Routing.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import SideBar from '../pages/SideBar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+const Routing = () => {
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route exact path='/' element={<SideBar />}></Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
+}
+
+export default Routing
---
+++ src/jsx/ProductMainPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductMainPage = () => {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default ProductMainPage
